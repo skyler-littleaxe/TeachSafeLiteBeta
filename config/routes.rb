e1f917400fd646e36983cf8ae62bad37624ad2fe
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-05-27 21:57:22 -0500

    created user session controller module, added auithenticate_user! to ApplicationController,  updated routes for url cosmetics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
   root to: "home#index"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
 end
